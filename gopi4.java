981acb7de4068a548eb5588a9db44aa4e11974e1
gopi5m1 <41737824+gopi5m1@users.noreply.github.com>
2018-07-27 14:41:02 +0530

    Create gopi4.java

---
+++ gopi4.java
@@ -0,0 +1,13 @@
+ int i=0;
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter a character : ");
+	char ch=sc.next( ).charAt(0);	 		
+	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+	{
+	System.out.println("Vowel"); 
+	}
+	else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
+		System.out.println("Entered character "+ch+" is Consonant");
+	      else
+		System.out.println("invalid");		
+	}
